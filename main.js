5fd0750914a178f09fe52b28ad28e1d92c0db2d1
Iustus Renidet <iustusrenidet@gmail.com>
2025-10-07 16:05:04 -0600

    icono

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 720,
+    backgroundColor: '#f3f6f1',
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, 'build', process.platform === 'win32' ? 'icon.ico' : 'icon.png')
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'vistas', 'Vista3.html'));
+};
+
+app.whenReady().then(() => {
+  app.setAppUserModelId('com.summa.cham');
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
