137b83cc80eedca8567f229deb1626c12e26f705
Iustus Renidet <iustusrenidet@gmail.com>
2025-12-24 13:40:25 -0600

    fix: NET RESULTS NORTHWEST ahora resta Member Centricity correctamente para Ciudad de México

--- vistas/js/resumen-view.js
+++ vistas/js/resumen-view.js
@@ -1283,7 +1283,7 @@
               "OPERATING RESULTS NOROESTE",
               "NO OPERATING RESULTS",
             ],
-            mc: totalesCero(),
+            mc: memberCentricityNw,
             other: otherNw,
             labels: [
               "NET RESULTS NORTHWEST",
