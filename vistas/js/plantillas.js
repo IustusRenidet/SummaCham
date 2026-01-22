7a2144eef424b024771ff1cbc03884fdec5388c5
Rintel Gatachi <130248633+IustusRenidet@users.noreply.github.com>
2026-01-22 10:39:00 -0600

    Retry plantillas init after DOM ready

--- vistas/js/plantillas.js
+++ vistas/js/plantillas.js
@@ -64,9 +64,13 @@
   let initDone = false;
   function init() {
     if (initDone) return;
+    cacheDOMElements();
+    if (!dom.moduloSelect || !dom.anioSelect || !dom.capituloSelect) {
+      setTimeout(init, 80);
+      return;
+    }
     initDone = true;
     try {
-      cacheDOMElements();
       bindEventListeners();
     } catch (error) {
       console.error("Error inicializando el gestor de plantillas:", error);
@@ -102,8 +106,6 @@
       setTimeout(ensureContext, 1200);
     });
   };
-
-  bootstrapEarly();
 
   function cacheDOMElements() {
     // Selectors
@@ -9508,6 +9510,8 @@
         saveContextToURL();
       };
     }
+
+    bootstrapEarly();
 
     // Cargar contexto al iniciar
     loadContextFromURL();
